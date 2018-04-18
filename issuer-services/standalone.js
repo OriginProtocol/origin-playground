7ffa98350e0cf9a383b6b403ef63038d3717e6f4
Nick Poulden <nick@poulden.com>
2018-04-17 19:16:45 -0600

    For SSL in production

--- issuer-services/standalone.js
+++ issuer-services/standalone.js
@@ -20,6 +20,15 @@ let Config = require('./config.json')
 Config.web3 = new Web3(Config.provider)
 
 const app = express()
+if (process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+      next()
+    }
+  })
+}
 app.use(serveStatic('public'))
 app.use(
   session({
